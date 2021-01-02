b4ea3ba833a2382bf8e2245cee6b4aca484a9597
James Thomas <james.l.thomas@sprint.com>
2021-01-02 14:06:09 -0600

    added auth subapp

---
+++ packages/auth/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+// To avoid class collisions in css
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'au',
+});
+
+export default ({ history, onSignIn }) => {
+    return (
+        <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <Router history={history}>
+                    <Switch>
+                        <Route path="/auth/signin">
+                            <Signin onSignIn={onSignIn} />
+                        </Route>
+                        <Route path="/auth/signup">
+                            <Signup onSignIn={onSignIn} />
+                        </Route>
+                    </Switch>
+                </Router>
+            </StylesProvider>
+        </div>
+    );
+};
