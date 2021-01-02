d37efb124acac924c9d851557f8877a8a9788c2f
James Thomas <james.l.thomas@sprint.com>
2021-01-02 10:58:37 -0600

    fixed navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,25 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { userHistory } from 'react-router-dom';
 
 // Should be able to use this with any of the frameworks
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+                if (pathname != nextPathname) {
+                    history.push(nextPathname);
+                }
+            },
+        });
+
+        history.listen(onParentNavigate);
+
+    }, []);
 
     return <div ref={ref} />;
 };
